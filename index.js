5e4e3b011b81ec32e5ac68787dbefdc5f85e4dd6
Allen Mordukhaev <allenmor93@gmail.com>
2022-07-18 21:03:46 -0400

    commit tree a6db8390c77c4a724182fc2b88587456459707e1

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(a){
   // Write your code here!
+  if (a <= 400) {
+    return 'This one is on me!';
+  } else if (a >= 400 && a <= 2000) {
+    return 'That will be twenty bucks.'
+  } else if (a > 2000 && a <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(b){
   // Write your code here!
+  return b === 'NYC'?'Ok, sounds good.':'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(c){
   // Write your code here!
+  switch(c) {
+    case 'generous':
+      return 'Thank you so much.'
+      case 'not as generous':
+        return 'Thank you.'
+        default:
+          return 'Bye.'
+  } 
 }
